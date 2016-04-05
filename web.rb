24461cdfbf3dbf08c638de777b911da90a38aba4
nicklegr <addr.for+github@gmail.com>
2016-04-05 22:53:41 +0900

    月収予測を追加

--- web.rb
+++ web.rb
@@ -1,11 +1,16 @@
 # coding: utf-8
 
+Encoding.default_external = Encoding::UTF_8
+
 require "bundler"
 Bundler.require(:default, :web)
 
 require_relative "db"
 
 get "/" do
+  @mona_jpy_bid_low = 7.0
+  @mona_jpy_bid_high = 8.0
+
   @earnings = Earning.desc(:created_at).limit(360)
 
   slim :index
